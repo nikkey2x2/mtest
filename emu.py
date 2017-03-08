c5b54f4e06804cdf2fd90d2906332b3a983bd4ab
nikkey2x2 <nik.chelny@gmail.com>
2017-03-08 15:58:21 +0300

    pep8

--- emu.py
+++ emu.py
@@ -6,18 +6,20 @@ G_EH = 1
 G_POP = 2
 G_NONE = 3
 GENRES = {G_RNB: 'RnB', G_EH: 'electrohouse', G_POP: 'pop'}
-ACTIONS = {G_RNB: 'moves his body and head back and forth, crouched, arms bent '
-                  'at the elbows',
-           G_EH: 'moves his body back and forth, doesnt move his head, his arms'
-                 'move in circles, his legs are dancing to the beat',
+ACTIONS = {G_RNB: 'moves his body and head back and forth, crouched, arms bent'
+                  ' at the elbows',
+           G_EH: 'moves his body back and forth, doesnt move his head, his '
+                 'arms move in circles, his legs are dancing to the beat',
            G_POP: 'slowly moves his body, head, legs and arms',
            G_NONE: 'goes to bar and drinks vodka'}
 
 
 class Club:
     playing = None
+
     def __init__(self):
         self.people = []
+
     def change_music(self, genre):
         if genre in GENRES:
             print('Club changed music to {0}!'.format(GENRES[genre]))
@@ -31,8 +33,10 @@ class Club:
 
 class Man:
     state = None
+
     def __init__(self, genres):
         self.genres = genres
+
     def change_state(self, genre):
         self.state = genre if genre in self.genres else G_NONE
 
